13453e2ba76120d14c22401525969a4b9f6b7133
Jhosep2022 <jose.condori.r@ucb.edu.bo>
2024-09-09 17:10:03 -0400

    creacio del proyecto react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">¡Tailwind CSS está funcionando!</h1>
+      <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
+        ¡Prueba Tailwind!
+      </button>
+    </div>
+    </>
+  )
+}
+
+export default App
